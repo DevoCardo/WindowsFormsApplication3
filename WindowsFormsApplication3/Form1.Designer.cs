1cc002cd24da43772958db2091aedef88d88ef25
temp <temp>
2016-09-29 12:51:56 -0400

    Add project files.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("OCR A Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(43, 80);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(394, 22);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Happy Constellation Day!";
+            // 
+            // subText
+            // 
+            this.subText.AutoSize = true;
+            this.subText.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subText.Location = new System.Drawing.Point(135, 123);
+            this.subText.Name = "subText";
+            this.subText.Size = new System.Drawing.Size(210, 23);
+            this.subText.TabIndex = 1;
+            this.subText.Text = "Click for YOUR constellation!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 295);
+            this.Controls.Add(this.subText);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subText;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Graphics fg = this.CreateGraphics();
+            //Font drawFont = new Font("Ariel", 50, FontStyle.Bold);
+            //SolidBrush 
+            //    firstBrush= new SolidBrush(Color.Black);
+            InitializeComponent();
+            //fg.DrawString("Greetings!", drawFont, firstBrush, 100, 100);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Gets rid of the title page completely
+            titleLabel.Visible = false;
+            subText.Visible = false;
+            Graphics formGraphics = this.CreateGraphics();
+            Pen whitePen = new Pen(Color.White, 10);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            //Creates stars and black background and sounds
+            formGraphics.Clear(Color.Black);
+            SoundPlayer player1 = new SoundPlayer(Properties.Resources.starSound);
+            player1.Play();
+            formGraphics.FillEllipse(whiteBrush, 297, 200, 10, 10);
+            Thread.Sleep(50);
+            SoundPlayer player2 = new SoundPlayer(Properties.Resources.starSound);
+            player2.Play();
+            formGraphics.FillEllipse(whiteBrush, 271, 238, 10, 10);
+            Thread.Sleep(50);
+            SoundPlayer player3 = new SoundPlayer(Properties.Resources.starSound);
+            player3.Play();
+            formGraphics.FillEllipse(whiteBrush, 212, 202, 10, 10);
+            Thread.Sleep(50);
+            SoundPlayer player4 = new SoundPlayer(Properties.Resources.starSound);
+            player4.Play();
+            formGraphics.FillEllipse(whiteBrush, 221, 162, 10, 10);
+            Thread.Sleep(50);
+            SoundPlayer player5 = new SoundPlayer(Properties.Resources.starSound);
+            player5.Play();
+            formGraphics.FillEllipse(whiteBrush, 193, 126, 10, 10);
+            Thread.Sleep(50);
+            SoundPlayer player6 = new SoundPlayer(Properties.Resources.starSound);
+            player6.Play();
+            formGraphics.FillEllipse(whiteBrush, 105, 68, 10, 10);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 105, 68, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 221 , 162 , 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 221, 162, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 271 , 238, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 271, 238, 297, 200);
+
+            Thread.Sleep(500);
+
+            Pen bluePen = new Pen(Color.Blue, 10);
+            SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+            formGraphics.FillEllipse(blueBrush, 297, 200, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(blueBrush, 271, 238, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(blueBrush, 212, 202, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(blueBrush, 221, 162, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(whiteBrush, 193, 126, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(blueBrush, 105, 68, 10, 10);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(bluePen, 105, 68, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(bluePen, 221, 162, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(bluePen, 221, 162, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(bluePen, 271, 238, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(bluePen, 271, 238, 297, 200);
+
+            Thread.Sleep(500);
+
+            Pen yellowPen = new Pen(Color.Yellow, 10);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+            formGraphics.FillEllipse(yellowBrush, 297, 200, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(yellowBrush, 271, 238, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(yellowBrush, 212, 202, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(yellowBrush, 221, 162, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(yellowBrush, 193, 126, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(yellowBrush, 105, 68, 10, 10);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(yellowPen, 105, 68, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(yellowPen, 221, 162, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(yellowPen, 221, 162, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(yellowPen, 271, 238, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(yellowPen, 271, 238, 297, 200);
+            Thread.Sleep(500);
+
+            Pen redPen = new Pen(Color.Red, 10);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+
+            formGraphics.FillEllipse(redBrush, 297, 200, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(redBrush, 271, 238, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(redBrush, 212, 202, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(redBrush, 221, 162, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(redBrush, 193, 126, 10, 10);
+            Thread.Sleep(50);
+            formGraphics.FillEllipse(redBrush, 105, 68, 10, 10);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(redPen, 105, 68, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(redPen, 221, 162, 208, 140);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(redPen, 221, 162, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(redPen, 271, 238, 212, 202);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(redPen, 271, 238, 297, 200);
+
+
+
+            Thread.Sleep(2000);
+            formGraphics.Clear(Color.Black);
+            Thread.Sleep(500);
+            //Creates Music / Band test and lightning bolt
+            Graphics fg = this.CreateGraphics();
+            Font drawFont = new Font("Ariel", 50, FontStyle.Bold);
+            SolidBrush bandBrush = new SolidBrush(Color.White);
+            fg.DrawString("Music", drawFont, bandBrush, 0, 40);
+            Thread.Sleep(500);
+            fg.DrawString("Band", drawFont, bandBrush, 250, 40);
+            Thread.Sleep(200);
+            formGraphics.DrawLine(whitePen, 220, 50, 210, 75 );
+            formGraphics.DrawLine(whitePen, 240, 75, 207, 75);
+            formGraphics.DrawLine(whitePen, 240, 75, 210, 110);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
